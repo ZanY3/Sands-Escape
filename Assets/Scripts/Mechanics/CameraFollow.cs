2b1c694fd040fada5f999cfd871029103e41fb9b
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-06-28 16:42:19 +0300

    Boss room bug fix

--- Assets/Scripts/Mechanics/CameraFollow.cs
+++ Assets/Scripts/Mechanics/CameraFollow.cs
@@ -8,6 +8,11 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     [Range(0.01f, 0.99f)] public float smoothness;
 
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target.gameObject.transform.position = new Vector3(0, -7, 0);
+    }
     private void LateUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothness);
